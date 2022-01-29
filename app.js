f527d03dfa24c98e1a1b70b6806cb7380005cb99
Fabian B <lince.86@gmail.com>
2022-01-28 20:52:12 -0500

    02.Setting Template Engine - ejs

--- app.js
+++ app.js
@@ -5,25 +5,33 @@ const express = require('express');
 // Set express app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
 
 // Listen for requests
 app.listen(3000);
 
-// Initial get requests
+
+// Get requests
 app.get('/', (req, res) =>{
-  res.sendFile('./views/index.html', {root: __dirname});
+  const blogs = [
+    {title: 'Yoshi Find Eggs', snippet: 'Magna aliqua exercitation aliquip ex incididunt est'},
+    {title: 'Mario find stars', snippet: 'Magna aliqua exercitation aliquip ex incididunt est'},
+    {title: 'How to defeat browser', snippet: 'Magna aliqua exercitation aliquip ex incididunt est'},
+  ];
+  res.render('index', {title: 'Home', blogs: blogs});
 });
 
 app.get('/about', (req, res) =>{
-  res.sendFile('./views/about.html', {root: __dirname});
+  res.render('about', {title: 'About'});
 });
 
-// Redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {title: 'Create a New Blog'});
 });
 
-// Always at the bottom
+// 404 Page. Always at the bottom
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', {root: __dirname});
+  res.status(404).render('404', {title: '404'});
 });
